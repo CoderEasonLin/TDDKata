8250bf582b3cbeac83ecd7cac5256739c3eab463
CoderEasonLin <lin07646@gmail.com>
2017-11-03 00:33:59 +0800

    init

---
+++ TennisKata/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TennisKata
+{
+    [TestFixture]
+    public class TennisKataTests
+    {
+        [Test]
+        public void GameStartScoreShouldBe0vs0()
+        {
+            var game = new TennisGame();
+
+            var expected = "0:0";
+
+            Assert.AreEqual(expected, game.Score());
+        }
+    }
+
+    public class TennisGame
+    {
+        public string Score()
+        {
+            return String.Empty;
+        }
+    }
+}
