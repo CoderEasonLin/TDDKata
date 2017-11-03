9961673df71d6b61c6d7668e792df8705c63d36c
CoderEasonLin <lin07646@gmail.com>
2017-11-03 08:07:43 +0800

    Test passed.

--- TennisKata/TennisGame.cs
+++ TennisKata/TennisGame.cs
@@ -6,6 +6,10 @@ namespace TennisKata
 {
     public class TennisGame
     {
+        private int player1Score { get; set; }
+        private bool player1Won;
+        private const int MAX_POINT_PER_GAME = 4;
+
         public string Score()
         {
             List<int> scoreMapping = new List<int>
@@ -16,14 +20,19 @@ namespace TennisKata
                 40
             };
 
+            if (player1Won)
+                return "Game Player1";
             return string.Format("{0}:0", scoreMapping[player1Score]);
         }
 
         public void Player1WinBall()
         {
             player1Score++;
+            if (player1Score == MAX_POINT_PER_GAME)
+            {
+                player1Won = true;
+            }
         }
 
-        private int player1Score { get; set; }
     }
 }
