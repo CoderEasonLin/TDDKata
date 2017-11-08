bf935bbbe415d50d87a0832ed0f74b722ef47170
CoderEasonLin <lin07646@gmail.com>
2017-11-09 00:39:54 +0800

    Refactory: Extract Frame.

--- BowlingKata/BowlingGame.cs
+++ BowlingKata/BowlingGame.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using NUnit.Framework;
+using NUnit.Framework.Api;
 
 namespace BowlingKata
 {
     public class BowlingGame
     {
+        private List<Frame> _frames;
+
         public int Score(string frameResult)
         {
             var score = 0;
@@ -12,31 +17,66 @@ namespace BowlingKata
                 return score;
 
             var frames = frameResult.Split(' ');
-            for (int i = 0; i < frames.Length; i++)
+            _frames = new List<Frame>();
+            foreach (var frame in frames)
             {
-                var frame = frames[i];
-                if (frame.IndexOf("/") == 1)
-                {
-                    score += 10;
+                _frames.Add(new Frame(frame));
+            }
 
-                    var nextFrame = frames[i + 1];
-                    if (nextFrame[0] == '-')
-                        continue;
-                    score += Convert.ToInt32(nextFrame[0].ToString());
-                }
-                else
-                {
-                    foreach (var token in frame)
-                    {
-                        if (token == '-')
-                            continue;
+            for (var i = 0; i < _frames.Count; i++)
+            {
+                var frame = _frames[i];
+                score += frame.Score;
 
-                        score += Convert.ToInt32(token.ToString());
-                    }
-                }
+                if (frame.IsSpare)
+                    score += _frames[i + 1].FirstRoll;
             }
 
             return score;
+        }
+    }
+
+    public class Frame
+    {
+        private readonly string _frame;
+
+        public Frame(string frame)
+        {
+            _frame = frame;
+        }
+
+        public int Score
+        {
+            get
+            {
+                if (_frame.IndexOf("/") == 1)
+                    return 10;
+
+                var score = 0;
+                foreach (var roll in _frame)
+                {
+                    score += ScoreOf(roll);
+                }
+                return score;
+            }
+        }
+
+        private static int ScoreOf(char token)
+        {
+            if (token == '-')
+                return 0;
+
+            return Convert.ToInt32(token.ToString());
+        }
+
+        public bool IsSpare
+        {
+            get { return _frame.IndexOf("/") == 1; }
+        }
+
+        public int FirstRoll
+        {
+            get { return ScoreOf(_frame[0]); }
         }
     }
 }
