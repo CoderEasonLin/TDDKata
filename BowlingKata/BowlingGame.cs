bf2c96e1a1e85febc41cfd9255dd952e3d09f1bd
CoderEasonLin <lin07646@gmail.com>
2017-11-08 09:48:44 +0800

    Add test: init score should be 0. remove unneccessary using.

--- BowlingKata/BowlingGame.cs
+++ BowlingKata/BowlingGame.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NUnit.Framework;
 
 namespace BowlingKata
 {
     public class BowlingGame
     {
+        public int Score()
+        {
+            return 0;
+        }
     }
+
+    [TestFixture]
+    public class BowlingGameTests
+    {
+        [Test]
+        public void BowlingGameInitScoreShouldBe0()
+        {
+            var game = new BowlingGame();
+
+            Assert.AreEqual(0, game.Score());
+        }
+    }
+
 }
