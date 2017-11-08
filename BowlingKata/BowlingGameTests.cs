ff91a88a8c125056dc9f46c7db78cb5432823f6f
CoderEasonLin <lin07646@gmail.com>
2017-11-09 01:11:53 +0800

    Update wrong test case...

--- BowlingKata/BowlingGameTests.cs
+++ BowlingKata/BowlingGameTests.cs
@@ -12,10 +12,10 @@ namespace BowlingKata
         [TestCase("15 1- 1/ 1- 18 1- 1- 1- 1- 1-", 33)]
         [TestCase("15 1- 1/ 1- 18 1/ 1- 1- 1- 1-", 43)]
         [TestCase("15 1- 1/ 1/ 18 1/ 1- 1- 1- 1-", 53)]
-        [TestCase("15 1- 1/ 1/ 18 1/ 1- 1- 1- 1/ 1", 64)]
-        [TestCase("15 1- 1/ 1/ 18 X 1- 1- 1- 1/ 1", 64)]
-        [TestCase("15 1- 1/ 1/ 18 X X 1- 1- 1/ 1", 84)]
-        [TestCase("15 1- 1/ 1/ 18 X X 1- 1- X 1/", 93)]
+        [TestCase("15 1- 1/ 1/ 18 1/ 1- 1- 1- 1/ 1", 63)]
+        [TestCase("15 1- 1/ 1/ 18 X 1- 1- 1- 1/ 1", 63)]
+        [TestCase("15 1- 1/ 1/ 18 X X 1- 1- 1/ 1", 83)]
+        [TestCase("15 1- 1/ 1/ 18 X X 1- 1- X 1/", 92)]
         public void BowlingGameTest(string rollResult, int expectedScore)
         {
             var game = new BowlingGame();
